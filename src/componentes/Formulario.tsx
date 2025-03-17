26f646b3b0bb4f4065e6d35dd7479cabd6be92d2
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-17 16:54:03 -0300

    Gerenciamento de estado e rotas

--- src/componentes/Formulario.tsx
+++ src/componentes/Formulario.tsx
@@ -1,8 +1,32 @@
-const Formulario = () => {
-    return (<form>
-        <input type="text" placeholder="Insira os nomes dos participantes"/>
-        <button disabled={true}>Adicionar</button>
-    </form>)
-}
+import React, { useRef, useState } from 'react';
 
-export default Formulario
+const Formulario = () => {
+    const [participantes, setParticipantes] = useState<string[]>([]);
+    const [nome, setNome] = useState<string>('');
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const adicionarParticipante = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (nome.trim()) {
+            setParticipantes([...participantes, nome]);
+            setNome('');
+            inputRef.current?.focus()
+        }
+    };
+
+    return (
+        <form onSubmit={adicionarParticipante}>
+            <input
+                ref={inputRef}
+                type="text"
+                placeholder="Insira os nomes dos participantes"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+            />
+            <button type="submit" disabled={!nome.trim()}>Adicionar</button>
+        </form>
+    );
+};
+
+export default Formulario;
